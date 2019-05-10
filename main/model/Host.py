cf644695ef60bc8c5e2cb847b83c8b3c0ab6b09a
dongmengyuan <dongmengyuan@xiyoulinux.org>
2019-05-10 15:14:59 +0800

    last

--- main/model/Host.py
+++ main/model/Host.py
@@ -6,6 +6,7 @@ class hosts(JsonSerializer,db.Model):
     ssh_username = db.Column(db.VARCHAR)
     ssh_password = db.Column(db.VARCHAR)
     ssh_port = db.Column(db.Integer)
+    host_name = db.Column(db.VARCHAR)
 
     def __str__(self):
-        return "host host_id is %s,host_ip: %s, ssh_username: %s,ssh_password : %s" % (self.id, self.host_ip,self.ssh_username,self.ssh_password)
+        return "host host_id is %s,host_ip: %s, ssh_username: %s,ssh_password : %s,ssh_port: %s,host_name: %s" % (self.id, self.host_ip,self.ssh_username,self.ssh_password,self.ssh_port,self.host_name)
